53f0a7990bfba3ec97d9d530ee4a30a6b6ce29b9
dekster15 <dekster0515@gmail.com>
2016-02-26 22:53:21 +0200

    Mat operation

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,30 @@
 public class Calculate{
    public static void main(String[] args){
 
-System.out.println("Calculate");
+       int a = Integer.valueOf(args[0]);
+       int b = Integer.valueOf(args[1]);
+      String oper = args[2];
+      double summ;
 
-}
+      if(oper.equals("plus")) {
+         summ = a + b;
+         System.out.println(oper + " " + summ);
+      }
+         else if(oper.equals("minus")) {
+            summ = a - b;
+            System.out.println(oper + " " + summ);
+         }
+
+      else if(oper.equals("div")) {
+         summ = a / b;
+         System.out.println(oper + " " + summ);
+      }
+      else if(oper.equals("multi")) {
+         summ = a * b;
+         System.out.println(oper + " " + summ);
+      }
+            else
+            System.out.println("Not operation!!!");
+
+      }
 }
